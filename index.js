0617ac1b7e666152f3807d395836205622152058
PEESMART62 <lawal5057@gmail.com>
2025-11-11 22:39:50 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+   const welcome = document.querySelector("#newspan");
+   const btnNew = document.querySelector("#newbtn");
+ 
+
+  btnNew.addEventListener("click", function(){
+    welcome.replaceWith("Welcome!");
+//   const newElement = document.createElement("span");
+//    newElement.textContent = "Welcome!";
+//    document.body.appendChild(newElement);
+   });
+
+
+
+
+
+
+
+const username = document.querySelector(".username");
+const password = document.querySelector(".username2");
+const btn = document.querySelector(".sign");
+
+
+btn.addEventListener("click", function () {
+    const usernameValue = username.value;
+    const passwordValue = password.value;
+
+  if (usernameValue == "appclickict" && passwordValue == "appclick1234") {
+      alert("User Login");
+  } else {
+      alert("Wrong Credentials");
+  }
+ });
+
+
+
+
+
+//  const btn = document.querySelector("button");
+//   const paragraph = document.querySelector("p");
+
+// btn.addEventListener("click", function(){
+//   paragraph.remove();
+// const newElement = document.createElement("h1");
+//  newElement.textContent = "Mr. Mike won't kill us with Javascript";
+//  document.body.appendChild(newElement);
+//  });
